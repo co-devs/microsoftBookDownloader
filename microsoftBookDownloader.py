8db688c7e536a1963eb7bf6684696bfe996285ff
Michael Devens <Michael Devens>
2017-07-16 20:46:09 -0400

    Workaround for redirects, using urllib2. Not my ideal solution but it seems to work and that's what matters. Changed script to iterate through whole list using website. Obviously, could be a LOT faster if it were multithreaded, but meh.

--- microsoftBookDownloader.py
+++ microsoftBookDownloader.py
@@ -4,13 +4,22 @@
 import os
 import requests
 import bs4
+import urllib2
+
+
+def downloadFile2(url):
+    result = urllib2.urlopen(url)
+    filename = os.path.basename(urllib2.urlparse.urlparse(result.url).path)
+    # print filename
+    # print result.geturl()
+    return result.geturl()
 
 
 def downloadFile(url, path):
     local_filename = path + url.split('/')[-1]
-    print url
-    print path
-    print local_filename
+    # print url
+    # print path
+    # print local_filename
     # NOTE the stream=True parameter
     r = requests.get(url, stream=True)
     with open(local_filename, 'wb') as f:
@@ -19,24 +28,24 @@ def downloadFile(url, path):
                 f.write(chunk)
     return local_filename
 
-# res = requests.get('https://blogs.msdn.microsoft.com/mssmallbiz/2017/07/11/largest-free-microsoft-ebook-giveaway-im-giving-away-millions-of-free-microsoft-ebooks-again-including-windows-10-office-365-office-2016-power-bi-azure-windows-8-1-office-2013-sharepo/?ranMID=24542&ranEAID=lw9MynSeamY&ranSiteID=lw9MynSeamY-ljYIUif9JQSw6mGEPRE6hg&tduid=(35cd2ef014e03b4e91ddad36b13d5d02)(256380)(2459594)(lw9MynSeamY-ljYIUif9JQSw6mGEPRE6hg)(')
-# res.raise_for_status()
-# soup = bs4.BeautifulSoup(res.text, "lxml")
+res = requests.get('https://blogs.msdn.microsoft.com/mssmallbiz/2017/07/11/largest-free-microsoft-ebook-giveaway-im-giving-away-millions-of-free-microsoft-ebooks-again-including-windows-10-office-365-office-2016-power-bi-azure-windows-8-1-office-2013-sharepo/?ranMID=24542&ranEAID=lw9MynSeamY&ranSiteID=lw9MynSeamY-ljYIUif9JQSw6mGEPRE6hg&tduid=(35cd2ef014e03b4e91ddad36b13d5d02)(256380)(2459594)(lw9MynSeamY-ljYIUif9JQSw6mGEPRE6hg)(')
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text, "lxml")
 
 # line = line.translate(None, '<>:\"/\|?*')
 badChars = '<>:\"/\|?*'
 baseDir = "C:\\Users\\mwill\\Documents\MicrosoftPubs\\"
 
-file = open('file.html')
-soup = bs4.BeautifulSoup(file, "lxml")
+# file = open('file.html')
+# soup = bs4.BeautifulSoup(file, "lxml")
 
 elems = soup.select('tbody')
 books = elems[0].select('tr')
 
 booksLen = len(books)
 
-# for i in xrange(1, booksLen):
-for i in xrange(1, 5):
+for i in xrange(1, booksLen):
+# for i in xrange(1, 5):
     bookData = books[i].select('td')
     category = bookData[0].getText().encode('ascii', 'ignore').translate(None, badChars)
     catDir = baseDir + category + '\\'
@@ -44,9 +53,9 @@ for i in xrange(1, 5):
     # if not, then make it
     try:
         os.stat(catDir)
-        print catDir, ' Exists'
+        # print catDir, ' Exists'
     except:
-        print 'Mkdir: ', catDir
+        # print 'Mkdir: ', catDir
         os.mkdir(catDir)
     # TODO: Debug print, remove or change to a progress meter
     # print 'Category: ', category
@@ -54,9 +63,9 @@ for i in xrange(1, 5):
     titleDir = catDir + title + '\\'
     try:
         os.stat(titleDir)
-        print titleDir, 'Exists'
+        # print titleDir, 'Exists'
     except:
-        print 'Mkdir: ', titleDir
+        # print 'Mkdir: ', titleDir
         os.mkdir(titleDir)
     # TODO: Debug print, remove or change to a progress meter
     # print 'Title: ', title
@@ -64,7 +73,10 @@ for i in xrange(1, 5):
     for j in links:
         # TODO: Debug prints (x2), remove or change to a progress meter
         # print j['href']
-        print "Downloading file to", titleDir
+        print 'Downloading file ' + str(i) + '/' + str(booksLen) + ' to', titleDir
         # TODO: Implement file download here. Download j['href']
+        # Will first need to check if file is present or if we are being
+        # redirected. We are likely being redirected and will need to download
+        # from a different url
         link = j['href']
-        downloadFile(link, titleDir)
+        downloadFile(downloadFile2(link), titleDir)
